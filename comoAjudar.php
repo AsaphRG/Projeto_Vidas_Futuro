b407630d9e680f86608c5d4370df2429d4ff3d63
Jéssica Coutinho <jessicamaiara.coutinho@gmail.com>
2023-01-16 08:28:16 -0300

    Criação da page home, começo da page como ajudar e adição do bootstrap icon.

---
+++ comoAjudar.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/imagens/logo.png" type="image/x-icon">
+
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="assets/css/styleGlobal.css">
+    <link rel="stylesheet" href="assets/css/pageComoAjudar.css">
+
+    <title>Instituto vidas pelo futuro</title>
+</head>
+<body>
+
+    <?php include 'header.php'?>
+    <main class="conteinerMasterComoAjudar">
+        <div class="conteinerConteudoComoAjudar">
+        <h1>Você pode ajudar o <b>Instituto vidas pelo futuro</b> das seguites formas: </h1>
+        </div>
+
+    </main>
+
+    <?php include 'footer.php'?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/imagens/logo.png" type="image/x-icon">
+
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.css">
+
+    <link rel="stylesheet" href="assets/css/pageIndex.css">
+
+    <title>Instituto vidas pelo futuro</title>
+</head>
+<body>
+    <footer class="conteinerMasterRodape">
+        <section class="contatos">
+            <h5>Contatos</h5>
+            <div class="iconesContatos">
+            <div class="conteinerEmail">
+                <a href="mailto:institutovidaspelofuturo@gmail.com">
+                <i class="bi bi-envelope-at"></i>
+                <p class="textoEmail">institutovidaspelofuturo@gmail.com</p>
+                </a>
+            </div>
+            <div class="conteinerInstagram">
+                <a href="http://www.instagram.com/institutovidaspelofuturo" target="_blank">
+                <i class="bi bi-instagram"></i>
+                <p class="textoInstagram">institutovidaspelofuturo</p>
+                </a>
+            </div>
+            <div class="conteinerTelefone">
+                <a href="tel:+11934588230">
+                <i class="bi bi-phone"></i>
+                <p class="textoTelefone">(11) 93458-8230</p>
+                </a>
+            </div>
+            <div>
+                
+            </div>
+            </div>
+        </section>
+        <section class="direitos">
+            <p>Instituto vidas pelo futuro 2023. Todos os direitos reservados&copy;.</p>
+        </section>
+
+    </footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
