2406a2f2213237bee17ce1062537e3269beb7900
AsaphRG <asaphresina1215@gmail.com>
2023-02-18 23:10:14 -0300

    Ajuste header, alteração nomes de index

--- header.php
+++ header.php
@@ -19,10 +19,10 @@
         <nav class="conteinerMenu">
             <a class="menuItem active" href="index.php">INÍCIO</a>
             <a class="menuItem" href="comoAjudar.php">COMO AJUDAR</a>
-            <a class="menuItem" href="#comoAtuamos">COMO ATUAMOS</a>
-            <a class="menuItem" href="">DOAR</a>
-            <a class="menuItem" href="">NOTÍCIAS</a>
-            <a class="menuItem" href="#parceiros">PARCEIROS</a>
+            <a class="menuItem" href="index.php#comoAtuamos">COMO ATUAMOS</a>
+            <a class="menuItem" href="doar.php">DOAR</a>
+            <a class="menuItem" href="noticias.php">NOTÍCIAS</a>
+            <a class="menuItem" href="index.php#parceiros">PARCEIROS</a>
             <a class="menuItem" href="">QUEM SOMOS</a>
         </nav>
     </header>
